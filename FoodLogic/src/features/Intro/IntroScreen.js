aee1b9157ab93c3db04b95ffc0a74a0792c34c62
buwaneka sirinath <bsirinath@gmail.com>
2020-04-28 21:07:46 +0530

    packages installed, pod created

---
+++ FoodLogic/src/features/Intro/IntroScreen.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from "react";
+import { View } from "react-native";
+
+class InfoScreen extends PureComponent{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
+
+export default InfoScreen;
---
+++ FoodLogic/src/navigation/NavigationStack.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { createAppContainer , createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Image , View, Dimensions } from "react-native";
